d4d4ddfe5100f118d0c5d3e25665726eca794830
Ashton Playsted <ashtonplaysted@gmail.com>
2017-06-11 16:25:11 -0700

    Fix bootstrap error

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
+const webpack = require('webpack');
 
 module.exports = {
   entry: './app/javascripts/app.js',
@@ -11,17 +12,14 @@ module.exports = {
     // Copy our app's index.html to the build folder.
     new CopyWebpackPlugin([
       { from: './app/index.html', to: "index.html" }
-    ])
+    ]),
+    new webpack.ProvidePlugin({
+      $: "jquery",
+      jQuery: "jquery"
+    })
   ],
   module: {
     rules: [
-      {
-       test: /\.css$/,
-       use: [ 'style-loader', 'css-loader' ]
-      }
-    ],
-    loaders: [
-      { test: /\.json$/, use: 'json-loader' },
       {
         test: /\.js$/,
         exclude: /(node_modules|bower_components)/,
@@ -30,6 +28,19 @@ module.exports = {
           presets: ['es2015'],
           plugins: ['transform-runtime']
         }
+      },
+      {
+       test: /\.css$/,
+       use: [ 'style-loader', 'css-loader' ]
+      },
+      { test: /\.json$/, use: 'json-loader' },
+      {
+        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        use: [ 'url-loader?limit=10000&mimetype=application/font-woff' ]
+      },
+      {
+        test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        use: [ 'file-loader' ]
       }
     ]
   }
