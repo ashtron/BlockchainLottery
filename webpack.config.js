634568c5f2c14fa2b93c5aeb9a7488d46dde2991
Ashton Playsted <ashtonplaysted@gmail.com>
2017-07-05 14:02:10 -0700

    Update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
   plugins: [
     // Copy our app's index.html to the build folder.
     new CopyWebpackPlugin([
-      { from: './app/index.html', to: "index.html" }
+      { from: './app/index.html', to: "index.html" },
     ]),
     new webpack.ProvidePlugin({
       $: "jquery",
